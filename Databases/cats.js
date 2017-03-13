f98726488f344a3bb9f71b5acb0220521318780c
elijahchen <code.elijahchen@gmail.com>
2017-03-12 21:15:19 -0400

    - Updated .save method with .create

--- Databases/cats.js
+++ Databases/cats.js
@@ -1,7 +1,7 @@
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://user:testapp@ds129050.mlab.com:29050/ecdata")
+mongoose.connect("mongodb://user:testapp@ds129050.mlab.com:29050/ecdata");
 
-//==========
+//========== SCHEMA START
 //Define pattern for data, expectation of format must be defined. Otherwise define a solution.
 var catSchema = new mongoose.Schema({
     name: String,
@@ -10,22 +10,43 @@ var catSchema = new mongoose.Schema({
 });
 //Build a complex model complete with methods used
 var Cat = mongoose.model("Cat", catSchema);
-//==========
+//========== SCHEMA END
 
 //Add a new cat to DB
-var bigbob = new Cat({
-    name: "Bigbob",
-    age: 11,
-    temperament: "Fiesty"
-});
+// var bigbob = new Cat({
+//     name: "Smallbob",
+//     age: 3,
+//     temperament: "Evil"
+// });
+//
+// bigbob.save(function (err, cat) {
+//     if(err){
+//         console.log("Error: "+ err);
+//     } else {
+//         console.log("We just assimilated the cat to the DB...");
+//         console.log(cat);
+//     }
+// });
 
-bigbob.save(function (err, cat) {
+Cat.create({
+    name: "Snow White",
+    age: 15,
+    temperament: "Bland"
+}, function (err, cat) {
     if(err){
-        console.log("Error: "+ err);
+        console.log(err);
     } else {
-        console.log("We just assimilated the cat to the DB...");
         console.log(cat);
     }
 });
 
 //Retrieve all cats from the DB and console.log each one
+Cat.find({}, function (err, cats) {
+    if(err){
+        console.log("ERROR");
+        console.log(err);
+    } else {
+        console.log("ALL THE CATS...");
+        console.log(cats);
+    }
+});
